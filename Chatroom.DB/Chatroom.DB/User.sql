4cdb2f8819cccdb23409f6c9e466b63ba5fb108a
jp_oliveira1991@hotmail.com <jp_oliveira1991@hotmail.com>
2021-12-25 15:05:17 -0300

    Basic Project Structure

---
+++ Chatroom.DB/Chatroom.DB/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(20) NULL, 
+    [Password] VARCHAR(128) NULL
+)
---
+++ Chatroom/Chatroom.DAO/Utils/CryptographyUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Chatroom.DAO
+{
+    public class CryptographyUtils
+    {
+        public string EncryptPassword(string password)
+        {
+            // Source: https://www.ti-enxame.com/pt/c%23/como-posso-sha512-uma-string-em-c/1068643647/
+            var bytes = Encoding.UTF8.GetBytes(password);
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                // Convert to text
+                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+                var hashedInputStringBuilder = new StringBuilder(128);
+
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+    }
+}
